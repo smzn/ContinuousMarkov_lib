1a9b37be35927a60c52eb7f1b2e69025bf5c3ed5
smzn <s.mzn.eng@gmail.com>
2021-06-18 12:07:34 +0900

    Add files via upload
    20210618 最適化を追加(値も良さそう)

--- ContinuousMarkov_lib.py
+++ ContinuousMarkov_lib.py
@@ -17,6 +17,17 @@ class ContinuousMarkov_lib:
         for i in range(len(self.q)):
             self.q[i][i] = np.sum(self.q[i]) * (-1)
         return self.q
+    
+    def getQ(self, mu):#最適化用
+        q = self.p.copy()
+        #推移率行列を求める
+        #(1)サービス率と推移確率との積をとる
+        for i in range(len(self.q)):
+            q[i] *= mu[i]
+        #(2)対角要素に行和のマイナス値を入れる
+        for i in range(len(q)):
+            q[i][i] = np.sum(q[i]) * (-1)
+        return q
 
     def getStationary_solve(self):#numpy.solveを使う場合
         #定常分布を求める
@@ -29,6 +40,17 @@ class ContinuousMarkov_lib:
         self.pi = solve(q1.T, right)
         return self.pi
     
+    def getStationary_opt(self, q):#最適化用
+        #定常分布を求める
+        q1 = q.copy()
+        #(3)最終列に1を代入
+        right = [0 for i in range(len(q))]
+        right[-1] = 1 #最後の要素のみ1にする
+        q1[:,-1] = 1 #最終列を1にする
+        #(4)連立方程式を解く πP=0 => P^tπ=0
+        pi = solve(q1.T, right)
+        return pi
+    
     def getStationary_inv(self):#逆行列を使う場合(Durret P165参照)
         #定常分布を求める
         q1 = self.q.copy()
@@ -40,3 +62,15 @@ class ContinuousMarkov_lib:
         inv_q1 = np.linalg.pinv(q1)
         self.pi = np.dot(right, inv_q1)
         return self.pi
+    
+    def getOptimize(self, mu):
+        q = self.getQ(mu)
+        pi = self.getStationary_opt(q)
+        opt_value = np.var(pi)
+        return opt_value
+    
+    def getConstraint(self,mu):#平均時間の合計に近くなるようにeq条件をつける
+        nearTs = 0.8#条件値
+        Ts = [1/i for i in mu]#平均時間
+        return nearTs - np.sum(Ts)
+        
--- ContinuousMarkov_main.py
+++ ContinuousMarkov_main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ContinuousMarkov_lib as mdl
 import utils
+import scipy.optimize
 
 #Durrett P165 Example 4.14 (Duke Basketball)
 mu = np.array([4.0, 15/2, 3.0, 10.0]) #サービス率
@@ -23,3 +24,11 @@ print('Solveでの定常分布 : {}'.format(pi_solve))
 pi_inv = clib.getStationary_inv()
 print('逆行列での定常分布 : {}'.format(pi_inv))
 
+#最適化
+cons = ({'type':'eq','fun':clib.getConstraint})#条件
+opt=scipy.optimize.minimize(clib.getOptimize,x0=mu,method='CG',constraints=cons)#,bounds=b1
+print(opt['x'])
+olib = mdl.ContinuousMarkov_lib(p, opt['x'])
+q = olib.getTransitionRate()
+pi_solve = olib.getStationary_solve()
+print('最適化後の定常分布 : {}'.format(pi_solve))
